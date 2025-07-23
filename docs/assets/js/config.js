b2fe67017d966a702db4e4589061e14ac37092d6
Silas <kammersi.to@gmail.com>
2025-07-23 20:12:36 +0200

    Delete docs directory

--- docs/assets/js/config.js
+++
@@ -1,5 +0,0 @@
-export const CONFIG = {
-    API_URL: 'http://localhost:3000',
-    UserId: 'd3819fbc-9212-49ba-b162-28ed35f228fa', // Temporär, bis auth Logik integriert
-    WorkspaceId: '2b059a79-2911-4ec9-899d-59dd1b01f582' // Temporär, bis auth Logik integriert
-}
